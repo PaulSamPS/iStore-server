f5a194567f36b22d60237f8795311ab1f394f79d
PaulSamPS <psamoylenko2@gmail.com>
2021-09-16 03:20:40 +0300

    структура проекта,тесты

---
+++ src/product/product.model.ts
@@ -0,0 +1,47 @@
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses'
+import { Prop } from '@typegoose/typegoose'
+
+class ProductCharacteristic {
+    @Prop()
+    name: string
+
+    @Prop()
+    value: string
+}
+
+export interface ProductModel extends Base {}
+
+export class ProductModel extends TimeStamps {
+    @Prop()
+    image: string
+
+    @Prop()
+    title: string
+
+    @Prop()
+    price: number
+
+    @Prop()
+    oldPrice: number
+
+    @Prop()
+    credit: number
+
+    @Prop()
+    calculatedRating: number
+
+    @Prop()
+    description: string
+
+    @Prop()
+    disAdvantages: string
+
+    @Prop({ type: () => [String] })
+    categories: string[]
+
+    @Prop({ type: () => [String] })
+    tags: string[]
+
+    @Prop({ type: () => [ProductCharacteristic], _id: false })
+    characteristics: ProductCharacteristic[]
+}
